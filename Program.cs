87d46b8a34c1c614b63609628a4181651fc1edd0
Elissssey <eliseev.rds@gmail.com>
2023-03-25 12:02:56 +0300

    Задали m

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+using System;
+
+class Program
+{
+static void Main(string[] args)
+{
+Console.Write("Enter m: ");
+int m = int.Parse(Console.ReadLine());

a44a5e49997e007aa5d485886f281648e8ddf7f7
Elissssey <eliseev.rds@gmail.com>
2023-03-25 12:04:00 +0300

    Задали n и функцию

--- Program.cs
+++ Program.cs
@@ -8,3 +8,9 @@ static void Main(string[] args)
 {
 Console.Write("Enter m: ");
 int m = int.Parse(Console.ReadLine());
+    Console.Write("Enter n: ");
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ackermann function({0}, {1}): {2}", m, n, Ackermann(m, n));
+}
+

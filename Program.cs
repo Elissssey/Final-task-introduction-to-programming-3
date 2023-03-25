ae60a877716ea3f96b7c2b138943984146d55e46
Elissssey <eliseev.rds@gmail.com>
2023-03-25 12:04:57 +0300

    Задали решение для задачи

--- Program.cs
+++ Program.cs
@@ -13,4 +13,18 @@ int m = int.Parse(Console.ReadLine());
 
     Console.WriteLine("Ackermann function({0}, {1}): {2}", m, n, Ackermann(m, n));
 }
-
+static int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
